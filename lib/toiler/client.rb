7e9094ee814818e2c600c6122c829a841a553546
Emiliano Ritiro <eritiro@gmail.com>
2015-06-08 21:07:10 -0300

    perform_async method

---
+++ lib/toiler/client.rb
@@ -0,0 +1,40 @@
+module Toiler
+  class Client
+    @@queues = {}
+    @@topics = {}
+
+    class << self
+      def queues(name)
+        @@queues[name.to_s] ||= Toiler::Queue.new(name, sqs)
+      end
+
+      def sns
+        @sns ||= Aws::SNS::Client.new(aws_client_options(:sns_endpoint))
+      end
+
+      def sns_arn
+        @sns_arn ||= SnsArn
+      end
+
+      def sqs
+        @sqs ||= Aws::SQS::Client.new(aws_client_options(:sqs_endpoint))
+      end
+
+      def topics(name)
+        @@topics[name.to_s] ||= Topic.new(name, sns)
+      end
+
+      attr_accessor :account_id
+      attr_writer :sns, :sqs, :sqs_resource, :sns_arn
+
+      private
+
+      def aws_client_options(service_endpoint_key)
+        explicit_endpoint = Toiler.options[:aws][service_endpoint_key]
+        options = {}
+        options[:endpoint] = explicit_endpoint unless explicit_endpoint.to_s.empty?
+        options
+      end
+    end
+  end
+end
