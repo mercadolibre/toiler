c0e68748dd8dc91fd291b686ef11c88560623615
Sebastian Schepens <sebastian.schepens@mercadolibre.com>
2015-06-11 12:16:51 -0300

    tweaks to fetcher

--- lib/toiler/actor/fetcher.rb
+++ lib/toiler/actor/fetcher.rb
@@ -15,7 +15,7 @@ module Toiler
         debug "Initializing Fetcher for queue #{queue}..."
         @queue = Toiler::Aws::Queue.new queue, client
         @wait = Toiler.options[:wait] || 20
-        @free_processors = 0
+        @free_processors = Concurrent::AtomicFixnum.new(0)
         @batch = Toiler.worker_class_registry[queue].batch?
         @visibility_timeout = @queue.visibility_timeout
         debug "Finished initializing Fetcher for queue #{queue}"
@@ -44,7 +44,8 @@ module Toiler
       end
 
       def processor_finished
-        tell Utils::ActorMessage.new :poll_messages if (@free_processors += 1) == 1
+        @free_processors.increment
+        reschedule_poll
       end
 
       def poll_messages
@@ -54,19 +55,19 @@ module Toiler
         }
 
         # AWS limits the batch size by 10
-        options[:max_number_of_messages] = (batch? || free_processors > FETCH_LIMIT) ? FETCH_LIMIT : free_processors
+        processors = free_processors.value
+        options[:max_number_of_messages] = (batch? || processors > FETCH_LIMIT) ? FETCH_LIMIT : processors
         debug "Fetcher #{queue.name} retreiving messages with options: #{options.inspect}..."
         msgs = queue.receive_messages options
         debug "Fetcher #{queue.name} retreived #{msgs.count} messages..."
-        return reschedule_poll if msgs.empty?
 
-        assign_messages msgs
+        assign_messages msgs unless msgs.empty?
 
         reschedule_poll
       end
 
       def reschedule_poll
-        tell Utils::ActorMessage.new :poll_messages if free_processors > 0
+        tell Utils::ActorMessage.new :poll_messages if free_processors.value > 0
       end
 
       def processor_pool
@@ -74,13 +75,13 @@ module Toiler
       end
 
       def assign_messages(messages)
-        debug "Fetcher assigning #{messages.count} for queue #{queue}"
+        debug "Fetcher assigning #{messages.count} for queue #{queue.name}"
         messages = [messages] if batch?
         messages.each do |m|
           processor_pool.tell Utils::ActorMessage.new(:process, [visibility_timeout, m])
-          @free_processors -= 1
+          @free_processors.decrement
         end
-        debug "Fetcher finished assigning #{messages.count} for queue #{queue}"
+        debug "Fetcher finished assigning #{messages.count} for queue #{queue.name}"
       end
     end
   end
