d5e991334fa04e4520d0729ff1b1396f543c3542
sschepens <sebastian.schepens@mercadolibre.com>
2015-07-07 01:55:19 -0300

    Cleanup Logging module

--- lib/toiler/utils/logging.rb
+++ lib/toiler/utils/logging.rb
@@ -3,9 +3,10 @@ require 'logger'
 
 module Toiler
   module Utils
+    # Initializes and exposes Toiler's default logger
     module Logging
+      # Toiler's default log formatter
       class Pretty < Logger::Formatter
-        # Provide a call() method that returns the formatted message.
         def call(severity, time, program_name, message)
           "#{time.utc.iso8601} Pid:#{Process.pid} Actor:#{program_name} Level:#{severity}: #{message.respond_to?(:gsub) ? message.gsub("\n", "\n\t") : message}\n"
         end
@@ -14,7 +15,7 @@ module Toiler
       module_function
 
       def initialize_logger(log_target = STDOUT)
-        log_target = STDOUT if log_target == nil
+        log_target = STDOUT if log_target.nil?
         @logger = Logger.new(log_target)
         @logger.level = Logger::INFO
         @logger.formatter = Pretty.new
